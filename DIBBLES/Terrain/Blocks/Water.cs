87f1a95c092f3edeffc7c301845293ee68dcbed6
DrBellubins <88703839+DrBellubins@users.noreply.github.com>
2025-09-13 04:18:43 -0400

    water

---
+++ DIBBLES/Terrain/Blocks/Water.cs
@@ -0,0 +1,10 @@
+namespace DIBBLES.Terrain.Blocks;
+
+public class Water
+{
+    // Runs per-block logic, but is not instanced with per-block persistent data.
+    public static void Tick(Block block, Chunk chunk)
+    {
+        
+    }
+}
