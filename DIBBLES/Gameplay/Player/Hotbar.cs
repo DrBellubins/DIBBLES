ca7f8d7cb5e133d7a61a13215351687dc1dc56d7
DrBellubins <88703839+DrBellubins@users.noreply.github.com>
2025-09-18 12:14:05 -0400

    rtjeh

--- DIBBLES/Gameplay/Player/Hotbar.cs
+++ DIBBLES/Gameplay/Player/Hotbar.cs
@@ -150,6 +150,7 @@ public class Hotbar
                     hotbarRect.Y + 0.1f * hotbarRect.Height,
                     hotbarRect.Height * 0.8f, hotbarRect.Height * 0.8f);
 
+                Raylib.DrawRectangle((int)itemDestRect.X, (int)itemDestRect.Y, (int)itemDestRect.Width, (int)itemDestRect.Height, Color.Red);
                 drawBlockCube2D(itemTexture, new Vector2(itemDestRect.X, itemDestRect.Y), itemDestRect.Width);
                 //Raylib.DrawTexturePro(itemTexture, itemOrigRect, itemDestRect, Vector2.Zero, 0.0f, Color.White);
             }
