4c511eaed55395da7a1276467c72e40a33a4c1da
DrBellubins <88703839+DrBellubins@users.noreply.github.com>
2025-09-22 19:03:17 -0400

    HUGE memory optimization

--- DIBBLES/Terrain/TerrainTypes.cs
+++ DIBBLES/Terrain/TerrainTypes.cs
@@ -37,7 +37,6 @@ public enum ChunkGenerationStage
 public class Chunk
 {
     public Vector3Int Position;
-    public Block[] Blocks; // Flat array for locality.
 
     public byte[] BlockTypes;
     public byte[] LightLevels;
@@ -50,7 +49,6 @@ public class Chunk
     public Chunk(Vector3Int pos)
     {
         Position = pos;
-        //Blocks = new Block[TerrainGeneration.ChunkSize * TerrainGeneration.ChunkSize * TerrainGeneration.ChunkSize];
         
         BlockTypes =  new byte[TerrainGeneration.ChunkSize * TerrainGeneration.ChunkSize * TerrainGeneration.ChunkSize];
         LightLevels =  new byte[TerrainGeneration.ChunkSize * TerrainGeneration.ChunkSize * TerrainGeneration.ChunkSize];
@@ -58,6 +56,12 @@ public class Chunk
     }
 
     // Helper for flat indexing
+    private int ToIndex(int x, int y, int z)
+    {
+        return x + TerrainGeneration.ChunkSize * (y + TerrainGeneration.ChunkSize * z);
+    }
+    
+    // Get block at position
     public Block GetBlock(int x, int y, int z)
     {
         if (x < 0 || x >= TerrainGeneration.ChunkSize ||
@@ -65,16 +69,42 @@ public class Chunk
             z < 0 || z >= TerrainGeneration.ChunkSize)
         {
             // Return Air block if out of bounds
-            return new Block(new Vector3Int(x, y, z), BlockType.Air);
+            return new Block(new Vector3Int(Position.X + x, Position.Y + y, Position.Z + z), BlockType.Air);
         }
 
-        int index = x + TerrainGeneration.ChunkSize * (y + TerrainGeneration.ChunkSize * z);
-        return Blocks[index];
+        int index = ToIndex(x, y, z);
+        
+        var position = new Vector3Int(Position.X + x, Position.Y + y, Position.Z + z);
+        var type = (BlockType)BlockTypes[index];
+        var biome = (TerrainBiome)Biomes[index];
+        var light = LightLevels[index];
+        
+        var info = BlockData.Prefabs[type];
+
+        // Construct Block with all info
+        return new Block(position, type)
+        {
+            Biome = biome,
+            LightLevel = light,
+            Info = info
+        };
     }
 
-    public void SetBlock(int x, int y, int z, Block data)
+    // Write Block's values back into arrays
+    public void SetBlock(int x, int y, int z, Block block)
     {
-        int index = x + TerrainGeneration.ChunkSize * (y + TerrainGeneration.ChunkSize * z);
-        Blocks[index] = data;
+        if (x < 0 || x >= TerrainGeneration.ChunkSize ||
+            y < 0 || y >= TerrainGeneration.ChunkSize ||
+            z < 0 || z >= TerrainGeneration.ChunkSize)
+        {
+            // Out of bounds, do nothing or throw exception
+            return;
+        }
+
+        int index = ToIndex(x, y, z);
+        BlockTypes[index] = (byte)block.Type;
+        Biomes[index] = (byte)block.Biome;
+        LightLevels[index] = block.LightLevel;
+        // Note: Info is not stored per-block, it's static in BlockData.Prefabs
     }
 }
