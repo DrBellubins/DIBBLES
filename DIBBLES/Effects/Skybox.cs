ceb14403694242440cce64085a8f60b85f2d8af1
DrBellubins <88703839+DrBellubins@users.noreply.github.com>
2026-02-25 16:45:39 -0500

    sgsdasfag

--- DIBBLES/Effects/Skybox.cs
+++ DIBBLES/Effects/Skybox.cs
@@ -32,7 +32,11 @@ public class Skybox
 
         graphics.SetVertexBuffer(vb);
         graphics.Indices = ib;
+        
+        var cameraPosition = GameScene.PlayerCharacter.Camera.Position.ToVector3();
+        var worldMatrix = Matrix.CreateTranslation(cameraPosition);
 
+        skyboxShader.SetValue("World", worldMatrix);
         skyboxShader.SetValue("View", GameScene.PlayerCharacter.Camera.View);
         skyboxShader.SetValue("Projection", GameScene.PlayerCharacter.Camera.Projection);
 
